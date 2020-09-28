7aaf1add6b1559059899c6c142c5536fc9a2b960
UK <sdnrzls@gmail.com>
2020-09-28 16:16:21 +0900

    Test

--- portfolio/portfolio/app/src/main/java/com/example/portfolio/MainActivity.java
+++ portfolio/portfolio/app/src/main/java/com/example/portfolio/MainActivity.java
@@ -2,13 +2,19 @@ package com.example.portfolio;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 public class MainActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,25 +25,42 @@ public class MainActivity extends AppCompatActivity {
         ImageView image[] = new ImageView[4];
         //이미지뷰 위젯 id 저장할 배열
         final Integer imageId[] = {R.id.ivTenet,R.id.ivMul,R.id.ivSo,R.id.ivBts};
+        final Integer imagedr[]={R.drawable.tenet,R.drawable.mul,R.drawable.sode,R.drawable.bts};
         //이미지뷰 제목 저장할 배열
         final String imgName[] = {"테넷","뮬란","검객","BTS"};
 
         for(int i=0; i<imageId.length; i++){
-            final int index =i;
-
+            final int index;
+            index = i;
             image[index] = (ImageView)findViewById(imageId[index]);
             image[index].setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    Intent intent = new Intent(getApplicationContext(),MovieActivity.class);
-                    intent.putExtra("ImageID",imageId[index]);
-                    intent.putExtra("ImageName",imgName[index]);
-                    startActivity(intent);
+                    final View dlgView = (View) View.inflate(MainActivity.this,R.layout.time,null);
+                    AlertDialog.Builder dg = new AlertDialog.Builder(MainActivity.this);
+                    dg.setTitle("영화 시간");
+                    dg.setView(dlgView);
+                    dg.setPositiveButton("시간선택완료", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            RadioGroup rg = (RadioGroup)dlgView.findViewById(R.id.rg);
+                            RadioButton rd = (RadioButton)dlgView.findViewById(rg.getCheckedRadioButtonId());
+                            String time = rd.getText().toString();
+
+                            Intent intent = new Intent(getApplicationContext(),MovieActivity.class);
+                            intent.putExtra("ImageDR",imagedr[index]);
+                            intent.putExtra("ImageName",imgName[index]);
+                            intent.putExtra("Time",time);
+                            startActivity(intent);
+                        }
+                    });
+                    dg.setNegativeButton("닫기",null);
+                    dg.show();
+
+
                 }
             });
         }
 
-
-
+        }
     }
-}
--- portfolio/portfolio/app/src/main/java/com/example/portfolio/MovieActivity.java
+++ portfolio/portfolio/app/src/main/java/com/example/portfolio/MovieActivity.java
@@ -1,20 +1,27 @@
 package com.example.portfolio;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Parcelable;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.GridView;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MovieActivity extends AppCompatActivity {
+    TextView tv1,time1;
+    ImageView iv1;
+    Intent intent;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -22,34 +29,34 @@ public class MovieActivity extends AppCompatActivity {
         setContentView(R.layout.movie);
         setTitle("좌석 정하기");
 
-        Intent intent = getIntent();
-//        int poster = intent.getIntExtra("ImageID");
+        tv1 = (TextView)findViewById(R.id.tv1);
+        time1 = (TextView)findViewById(R.id.time1);
+        iv1 =(ImageView)findViewById(R.id.iv1);
 
-
-        TextView tv1 = (TextView)findViewById(R.id.tv1);
-        ImageView iv1 =(ImageView)findViewById(R.id.iv1);
-
+        intent = getIntent();
+        tv1.setText(intent.getStringExtra("ImageName"));
+        iv1.setImageResource(intent.getIntExtra("ImageDR",0));
+        time1.setText(intent.getStringExtra("Time"));
 
         GridView gv = (GridView)findViewById(R.id.gridView1);
+        MyGridAdapter gAdapter = new MyGridAdapter(this);
+        gv.setAdapter(gAdapter);
 
     }
 
     public class MyGridAdapter extends BaseAdapter{
         Context context;
         //이미지파일
-        Integer[] Seat ={R.drawable.seat1,R.drawable.seat2,R.drawable.seat3,R.drawable.seat4,
-                        R.drawable.seat5,R.drawable.seat6,R.drawable.seat7,R.drawable.seat8,
-                        R.drawable.seat1,R.drawable.seat2,R.drawable.seat3,R.drawable.seat4,
-                        R.drawable.seat5,R.drawable.seat6,R.drawable.seat7,R.drawable.seat8,
-                        R.drawable.seat1,R.drawable.seat2,R.drawable.seat3,R.drawable.seat4,
-                        R.drawable.seat5,R.drawable.seat6,R.drawable.seat7,R.drawable.seat8,
-                        R.drawable.seat1,R.drawable.seat2,R.drawable.seat3,R.drawable.seat4,
-                        R.drawable.seat5,R.drawable.seat6,R.drawable.seat7,R.drawable.seat8};
+        Integer[] Seat ={   R.drawable.seat1,R.drawable.seat2,R.drawable.seat3,R.drawable.seat4,
+                            R.drawable.seat5,R.drawable.seat6,R.drawable.seat7,R.drawable.seat8,
+                            R.drawable.seat1,R.drawable.seat2,R.drawable.seat3,R.drawable.seat4,
+                            R.drawable.seat5,R.drawable.seat6,R.drawable.seat7,R.drawable.seat8,
+                            R.drawable.seat1,R.drawable.seat2,R.drawable.seat3,R.drawable.seat4,
+                            R.drawable.seat5,R.drawable.seat6,R.drawable.seat7,R.drawable.seat8};
         //좌석이름
         String[] SeatNum = {"A1","A2","A3","A4","A5","A6","A7","A8",
                             "B1","B2","B3","B4","B5","B6","B7","B8",
-                            "C1","C2","C3","C4","C5","C6","C7","C8",
-                            "D1","D2","D3","D4","D5","D6","D7","D8"};
+                            "C1","C2","C3","C4","C5","C6","C7","C8"};
 
 
         public MyGridAdapter(Context c){
@@ -72,16 +79,50 @@ public class MovieActivity extends AppCompatActivity {
         }
 
         @Override
-        public View getView(int position, View convertView, ViewGroup parent) {
+        public View getView(final int position, View convertView, ViewGroup parent) {
             ImageView imageView = new ImageView(context);
-            imageView.setLayoutParams(new GridView.LayoutParams(100,100));
+            imageView.setLayoutParams(new GridView.LayoutParams(60,100));
             imageView.setScaleType(ImageView.ScaleType.FIT_CENTER);
             imageView.setPadding(5,5,5,5);
-
             imageView.setImageResource(Seat[position]);
 
 
-            return null;
+            imageView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    View dialogView = (View) View.inflate(MovieActivity.this,R.layout.dialog,null);
+
+                    AlertDialog.Builder dlg = new AlertDialog.Builder(MovieActivity.this);
+                    dlg.setTitle(SeatNum[position]+"열 예매 하시겠습니까?");
+                    dlg.setView(dialogView);
+                    dlg.setPositiveButton("예매", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            View dView = (View) View.inflate(MovieActivity.this,R.layout.check,null);
+                            AlertDialog.Builder dlg2 = new AlertDialog.Builder(MovieActivity.this);
+
+                            TextView tvlast1 = (TextView)dView.findViewById(R.id.tvlast1);
+                            TextView tvlast2 = (TextView)dView.findViewById(R.id.tvlast2);
+
+                            tvlast1.setText(intent.getStringExtra("Time"));
+                            tvlast2.setText(SeatNum[position]+"열 좌석 예약 완료 되었습니다");
+
+                            ImageView ivlast = (ImageView)dView.findViewById(R.id.ivlast);
+                            ivlast.setImageResource(intent.getIntExtra("ImageDR",0));
+
+                            dlg2.setTitle("예매 완료");
+                            dlg2.setView(dView);
+                            dlg2.setNegativeButton("닫기",null);
+                            dlg2.show();
+                        }
+                    });
+                    dlg.setNegativeButton("닫기",null);
+                    dlg.show();
+
+                }
+            });
+
+            return imageView;
         }
     }
 }
