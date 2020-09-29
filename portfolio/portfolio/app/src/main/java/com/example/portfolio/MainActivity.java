d9d96eea01534d150325454e1bce32a63ddaaf7b
UK <sdnrzls@gmail.com>
2020-09-29 15:01:22 +0900

    portfolio

--- portfolio/portfolio/app/src/main/java/com/example/portfolio/MainActivity.java
+++ portfolio/portfolio/app/src/main/java/com/example/portfolio/MainActivity.java
@@ -25,10 +25,12 @@ public class MainActivity extends AppCompatActivity {
         ImageView image[] = new ImageView[4];
         //이미지뷰 위젯 id 저장할 배열
         final Integer imageId[] = {R.id.ivTenet,R.id.ivMul,R.id.ivSo,R.id.ivBts};
+        //이미지뷰 drawable 저장할 배열 설정(이미지 자체를 넘겨주기 위해)
         final Integer imagedr[]={R.drawable.tenet,R.drawable.mul,R.drawable.sode,R.drawable.bts};
         //이미지뷰 제목 저장할 배열
         final String imgName[] = {"테넷","뮬란","검객","BTS"};
 
+        //이미지 위젯 설정과 클릭이벤트 사용을위해 for문 생성
         for(int i=0; i<imageId.length; i++){
             final int index;
             index = i;
@@ -36,18 +38,27 @@ public class MainActivity extends AppCompatActivity {
             image[index].setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
+                    //이미지 클릭시 타임.xml을 inflate한 다이얼로그 뷰! 생성
                     final View dlgView = (View) View.inflate(MainActivity.this,R.layout.time,null);
+                    //다이얼로그 생성
                     AlertDialog.Builder dg = new AlertDialog.Builder(MainActivity.this);
                     dg.setTitle("영화 시간");
+                    //다이얼로그에 뷰 지정
                     dg.setView(dlgView);
+                    //버튼 이벤트
                     dg.setPositiveButton("시간선택완료", new DialogInterface.OnClickListener() {
                         @Override
                         public void onClick(DialogInterface dialog, int which) {
+                            //라디오그룹 위젯 연결
                             RadioGroup rg = (RadioGroup)dlgView.findViewById(R.id.rg);
+                            //라디오 그룹에 체크되어있는 id만 라디오 버튼위젯 연결
                             RadioButton rd = (RadioButton)dlgView.findViewById(rg.getCheckedRadioButtonId());
+                            //체크되어있던 라디오버튼의 text를 time변수에 지정
                             String time = rd.getText().toString();
-
+                            //인텐트 설정 (현위치,보낼위치)
                             Intent intent = new Intent(getApplicationContext(),MovieActivity.class);
+                            //인텐트로 보낼 값 설정 (클릭한 image의 이름배열과 이미지주소배열 그리고 라디오텍스트)
+                            //("보낼값의 이름",값)
                             intent.putExtra("ImageDR",imagedr[index]);
                             intent.putExtra("ImageName",imgName[index]);
                             intent.putExtra("Time",time);
--- portfolio/portfolio/app/src/main/java/com/example/portfolio/MovieActivity.java
+++ portfolio/portfolio/app/src/main/java/com/example/portfolio/MovieActivity.java
@@ -3,13 +3,15 @@ package com.example.portfolio;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
 import android.os.Bundle;
-import android.os.Parcelable;
+
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+
 import android.widget.Button;
-import android.widget.CheckBox;
 import android.widget.GridView;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -22,28 +24,90 @@ public class MovieActivity extends AppCompatActivity {
     TextView tv1,time1;
     ImageView iv1;
     Intent intent;
+    Button btnSeat;
+    //좌석 배열값을 받을 배열생성
+    String[] seatNumArr = new String[24];
+    //배열 초기화 구분을위해 전체 값을 "-"로 설정
+    public MovieActivity(){
+        for(int i=0; i<seatNumArr.length; i++){
+            seatNumArr[i] = "-";
+        }
+    }
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.movie);
         setTitle("좌석 정하기");
-
+        //movie.xml의 위젯과 연결
         tv1 = (TextView)findViewById(R.id.tv1);
         time1 = (TextView)findViewById(R.id.time1);
         iv1 =(ImageView)findViewById(R.id.iv1);
-
+        btnSeat = (Button)findViewById(R.id.btnSeat);
+        //인텐트 받는 함수
         intent = getIntent();
+        //변수에 인텐트로 받은 값들 지정
         tv1.setText(intent.getStringExtra("ImageName"));
         iv1.setImageResource(intent.getIntExtra("ImageDR",0));
         time1.setText(intent.getStringExtra("Time"));
 
+        //그리드뷰 위젯 연결
         GridView gv = (GridView)findViewById(R.id.gridView1);
+        //그리드뷰 생성(public class MyGridAdapter extends BaseAdapter)
         MyGridAdapter gAdapter = new MyGridAdapter(this);
+        //그리드뷰 연결
         gv.setAdapter(gAdapter);
 
-    }
+        //버튼 클릭시
+        btnSeat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                View dialogView = (View) View.inflate(MovieActivity.this,R.layout.dialog,null);
 
+                AlertDialog.Builder dlg = new AlertDialog.Builder(MovieActivity.this);
+
+                //좌석 배열 구분 을 위한 for 문 사용
+                //String형의 공백 변수 생성 ("-"아닌 값을 받을)
+                String strSeatNumArr = "";
+                for(int i=0; i<seatNumArr.length; i++){
+                    if(!seatNumArr[i].equals("-")){
+                        //배열값이 "-"이 아니면 strSeatNumArr에 배열값 넣음
+                        strSeatNumArr = strSeatNumArr+ " "+ seatNumArr[i];
+                    }
+                }
+
+                dlg.setTitle(strSeatNumArr+"열 예매 하시겠습니까?");
+                dlg.setView(dialogView);
+
+                final String finalStrSeatNumArr = strSeatNumArr;
+                dlg.setPositiveButton("예매", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        View dView = (View) View.inflate(MovieActivity.this,R.layout.check,null);
+                        AlertDialog.Builder dlg2 = new AlertDialog.Builder(MovieActivity.this);
+
+                        TextView tvlast1 = (TextView)dView.findViewById(R.id.tvlast1);
+                        TextView tvlast2 = (TextView)dView.findViewById(R.id.tvlast2);
+
+                        tvlast1.setText(intent.getStringExtra("Time"));
+                        tvlast2.setText(finalStrSeatNumArr +"열 좌석 예약 완료 되었습니다");
+
+                        ImageView ivlast = (ImageView)dView.findViewById(R.id.ivlast);
+                        ivlast.setImageResource(intent.getIntExtra("ImageDR",0));
+
+                        dlg2.setTitle("예매 완료");
+                        dlg2.setView(dView);
+                        dlg2.setNegativeButton("닫기",null);
+                        dlg2.show();
+                    }
+                });
+                dlg.setNegativeButton("닫기",null);
+                dlg.show();
+            }
+        });
+
+    }
+    //BaseAdapter를 상속받는 MyGridAdapter class 생성
     public class MyGridAdapter extends BaseAdapter{
         Context context;
         //이미지파일
@@ -58,7 +122,7 @@ public class MovieActivity extends AppCompatActivity {
                             "B1","B2","B3","B4","B5","B6","B7","B8",
                             "C1","C2","C3","C4","C5","C6","C7","C8"};
 
-
+        //
         public MyGridAdapter(Context c){
             context = c;
         }
@@ -80,48 +144,22 @@ public class MovieActivity extends AppCompatActivity {
 
         @Override
         public View getView(final int position, View convertView, ViewGroup parent) {
-            ImageView imageView = new ImageView(context);
+            final ImageView imageView = new ImageView(context);
             imageView.setLayoutParams(new GridView.LayoutParams(60,100));
             imageView.setScaleType(ImageView.ScaleType.FIT_CENTER);
             imageView.setPadding(5,5,5,5);
             imageView.setImageResource(Seat[position]);
-
-
+            //이미지뷰 클릭 리스너
             imageView.setOnClickListener(new View.OnClickListener() {
+
                 @Override
                 public void onClick(View v) {
-                    View dialogView = (View) View.inflate(MovieActivity.this,R.layout.dialog,null);
-
-                    AlertDialog.Builder dlg = new AlertDialog.Builder(MovieActivity.this);
-                    dlg.setTitle(SeatNum[position]+"열 예매 하시겠습니까?");
-                    dlg.setView(dialogView);
-                    dlg.setPositiveButton("예매", new DialogInterface.OnClickListener() {
-                        @Override
-                        public void onClick(DialogInterface dialog, int which) {
-                            View dView = (View) View.inflate(MovieActivity.this,R.layout.check,null);
-                            AlertDialog.Builder dlg2 = new AlertDialog.Builder(MovieActivity.this);
-
-                            TextView tvlast1 = (TextView)dView.findViewById(R.id.tvlast1);
-                            TextView tvlast2 = (TextView)dView.findViewById(R.id.tvlast2);
-
-                            tvlast1.setText(intent.getStringExtra("Time"));
-                            tvlast2.setText(SeatNum[position]+"열 좌석 예약 완료 되었습니다");
-
-                            ImageView ivlast = (ImageView)dView.findViewById(R.id.ivlast);
-                            ivlast.setImageResource(intent.getIntExtra("ImageDR",0));
-
-                            dlg2.setTitle("예매 완료");
-                            dlg2.setView(dView);
-                            dlg2.setNegativeButton("닫기",null);
-                            dlg2.show();
-                        }
-                    });
-                    dlg.setNegativeButton("닫기",null);
-                    dlg.show();
-
+                    //이미지 클릭시 색 변경
+                    imageView.setColorFilter(Color.parseColor("#ffff0000"), PorterDuff.Mode.SRC_OVER);
+                    //클릭한 이미지의 SeatNum배열값을 전역변수 seatNumArr에 대입
+                    seatNumArr[position] = SeatNum[position];
                 }
             });
-
             return imageView;
         }
     }
